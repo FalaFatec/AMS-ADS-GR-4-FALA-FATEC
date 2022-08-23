8b5aaa1ebbdd06d5dcb1da3e22f1cd3e35864227
ViniMazon19 <vinicius.viturino@fatec.sp.gov.br>
2022-08-23 00:56:19 -0300

    delete

--- src/main/java/com/example/fala_fatec/repository/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fala_fatec.repository;
-
-import com.example.fala_fatec.entity.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-}
