222f527541dd5f04a1dd2848ecab5b19e71fc965
ViniMazon19 <vinicius.viturino@fatec.sp.gov.br>
2022-08-23 00:32:02 -0300

    a

--- src/main/java/com/example/falafatec/repository/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.falafatec.repository;
-
-import com.example.falafatec.entity.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-}
