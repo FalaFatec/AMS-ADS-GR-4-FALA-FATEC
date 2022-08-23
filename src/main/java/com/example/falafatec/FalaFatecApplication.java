a3bc5aee91e065b85d3d93b638f0294b22367567
ViniMazon19 <vinicius.viturino@fatec.sp.gov.br>
2022-08-23 01:12:10 -0300

    sign up

---
+++ src/main/java/com/example/falafatec/FalaFatecApplication.java
@@ -0,0 +1,13 @@
+package com.example.falafatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalaFatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalaFatecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/falafatec/FalaFatecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.falafatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalaFatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
