5a168a1844b16a454a45879ff1c9524427525408
ViniMazon19 <vinicius.viturino@fatec.sp.gov.br>
2022-11-08 12:52:17 -0300

    Update

---
+++ src/main/java/br/com/pifalafatec/controller/FileController.java
@@ -0,0 +1,53 @@
+package br.com.pifalafatec.controller;
+
+import br.com.pifalafatec.SaveFileResult;
+import lombok.var;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/files")
+public class FileController {
+
+    private String uploadLocation;
+
+    public FileController(@Value("${upload.location}") String uploadLocation) throws IOException {
+        this.uploadLocation = uploadLocation;
+        var uploadPath = Paths.get(uploadLocation);
+        if(!Files.exists(uploadPath)) {
+            Files.createDirectory(uploadPath);
+        }
+
+    }
+
+    public List<SaveFileResult> upload(@RequestPart List<MultipartFile> files){
+         return files.stream()
+                 .map(this::saveFile)
+                 .collect(Collectors.toList());
+    }
+
+    private SaveFileResult saveFile(MultipartFile file){
+        var filename = file.getOriginalFilename();
+        var dest = Paths.get(uploadLocation + "/" + filename);
+        try {
+            Files.copy(file.getInputStream(), dest, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new SaveFileResult().setFilename(filename).setError(true);
+        }
+        return new SaveFileResult().setFilename(filename);
+    }
+}
